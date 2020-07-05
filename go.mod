5a5e68594d845588f98e2647e201ba585a15ccb8
hjx0 <mrhjx2@gmail.com>
2020-07-05 11:01:18 +0800

    init all

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mrhjx2/start-basic
+
+go 1.13
+
+require golang.org/x/tour v0.0.0-20200201212631-8f38c9a8d074
---
+++ short-variable-declarations.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 3, 5
+	k := 6
+	c, python, java := true, false, "yes!"
+	fmt.Println(i, j, k, c, python, java)
+}
